4d0154521710afcf5649bf5b5c880c0cef96fa40
Jesriel <layugjesriel61@gmail.com>
2024-01-15 15:47:24 +0800

    Adding "MyDockets" page.

---
+++ Docket.Client/Components/EditableDocketCardBase.cs
@@ -0,0 +1,10 @@
+﻿using Docket.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Docket.Client.Components
+{
+    public class EditableDocketCardBase : ComponentBase
+    {
+        [Parameter] public IEnumerable<DTODocket> Dockets { get; set; }
+    }
+}
---
+++ Docket.Client/Utilities/TextModification.cs
@@ -0,0 +1,13 @@
+﻿namespace Docket.Client.Utilities
+{
+    public static class TextModification
+    {
+        public static string TextLimit(string input, int maxLength)
+        {
+            if (input.Length <= maxLength)
+                return input;
+
+            return input.Substring(0, maxLength) + "...";
+        }
+    }
+}
